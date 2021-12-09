9c1d85fa773dd014b411e250833d42adf733fb22
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2021-12-09 22:45:05 +0530

    Add files via upload

---
+++ Exercise/Array1.java
@@ -0,0 +1,14 @@
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] percentage= {54.56,60.66,75.35,59.65};
+		
+		for(int i=0;i<percentage.length;i++) {
+			System.out.println(percentage[i]+" ");
+		}
+
+	}
+
+}
