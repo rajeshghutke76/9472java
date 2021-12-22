dd13400f467960f0e36a863498b8d0efd24b1645
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2021-12-22 21:52:41 +0530

    Add files via upload

---
+++ Exercise/22-12-2021/Program9.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Program9 {
+	public static void main(String[] args)
+	{
+		String arr[] = { "practice.geeksforgeeks.org",
+						"quiz.geeksforgeeks.org",
+						"code.geeksforgeeks.org" };
+		Arrays.sort(arr);
+		System.out.println("Modified arr[] : \n%s\n\n");
+						Arrays.toString(arr);
+		Arrays.sort(arr, Collections.reverseOrder());
+		System.out.println("Modified arr[] : \n%s\n\n");
+						Arrays.toString(arr);
+	}
+}
+
+
+
