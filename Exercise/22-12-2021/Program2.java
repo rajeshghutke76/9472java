d3808eff7b0c6c57aab03d38cc0b6305cfdc38c1
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2021-12-22 19:21:55 +0530

    Add files via upload

---
+++ Exercise/22-12-2021/Program2.java
@@ -0,0 +1,13 @@
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'c';
+		int num = 88;
+		float f=num;
+		System.out.println("char value is"+ ch);
+		System.out.println("int value is " + num);
+	    System.out.println("Flot value is" + f);
+}
+ }
