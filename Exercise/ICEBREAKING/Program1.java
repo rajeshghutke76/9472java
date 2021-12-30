1e6c8562b9442a9fa71a02d1cb627aaf6a62a220
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2021-12-30 17:37:22 +0530

    Add files via upload

---
+++ Exercise/ICEBREAKING/Program1.java
@@ -0,0 +1,20 @@
+
+ public class Program1 {
+  public static void main(String[] args) {
+		 for(int i=20;i>=0;i--){
+	            if((i==7)||(i==15)){
+	                continue;
+	            }
+	            else{
+	                System.out.println(i);	
+
+	}
+   }
+  }
+ }
+ 
+ /*
+ output:
+ 
+ */
+ 
