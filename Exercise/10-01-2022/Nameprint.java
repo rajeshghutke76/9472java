051f7ad2b6a1c2529ca2938e57048d83ca6205e9
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-10 14:01:22 +0530

    Add files via upload

---
+++ Exercise/10-01-2022/Nameprint.java
@@ -0,0 +1,10 @@
+package com.Anudip.Rajesh;
+
+public class Nameprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Hello world");
+	}
+
+}
