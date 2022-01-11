59b2671779f780e6268a7b9cad7aae789a22c52e
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-11 23:40:27 +0530

    Add files via upload

---
+++ Exercise/W3 Resources/Addition.java
@@ -0,0 +1,16 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+	    System.out.println("addition start:");
+		
+		System.out.println(25+25);
+		System.out.println(50+50);
+		System.out.println(24+26);
+		System.out.println(100+100);
+
+	    System.out.println("addition end");
+		
+	}
+
+}
