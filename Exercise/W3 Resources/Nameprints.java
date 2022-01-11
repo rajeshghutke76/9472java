cdccd6a1698c05b5ea8c69f5b2645e56537a1f94
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-11 23:12:35 +0530

    Add files via upload

---
+++ Exercise/W3 Resources/Nameprints.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+public class Nameprints {
+
+	public static void main(String[] args) {
+      Scanner input= new Scanner(System.in);
+      System.out.println("Input  the first name:");
+      String fname=input.next();
+	  System.out.println("Input  the midle name:");
+	  String mname=input.next();
+      System.out.println("Input  the last name:");
+	  String lname=input.next();
+	  System.out.println("Hello \n"+ fname +" "+ mname +" "+ lname);
+		 
+	}
+
+}
+/*
+Output:
+
+*/
