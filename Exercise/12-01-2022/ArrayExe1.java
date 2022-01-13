73279eb280538827ed9566f0602ba483aa2174f6
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-13 15:24:16 +0530

    Add files via upload

---
+++ Exercise/12-01-2022/ArrayExe1.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayExe1 {
+
+	public static void main(String[] args) {
+		
+		List<String> list_String= new ArrayList<String>();
+		
+		list_String.add("Black");
+		list_String.add("Green");
+		list_String.add("Orange");
+        list_String.add("Red");
+        list_String.add("White");
+        
+        System.out.println(list_String);
+	}
+
+}
+/*
+OUTPUT:
+[Black, Green, Orange, Red, White]
+
+*/
