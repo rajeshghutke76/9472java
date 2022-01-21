5beb32376b59b6822f1a95a8c489dd3c3564ef7c
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-22 01:00:10 +0530

    Update ExampleArray2.java

--- Exercise/16-01-2022/ExampleArray2.java
+++ Exercise/16-01-2022/ExampleArray2.java
@@ -7,5 +7,28 @@ public static void main(String[] args) {
 System.out.println("Element at index "+i+" :"+arr[i]);
 }
 }
-
 }
+/*
+Output:
+Element at index 0 :1
+Element at index 1 :2
+Element at index 2 :3
+Element at index 3 :4
+Element at index 4 :5
+Element at index 5 :6
+Element at index 6 :7
+Element at index 7 :8
+Element at index 8 :9
+Element at index 9 :10
+Element at index 10 :11
+Element at index 11 :12
+Element at index 12 :13
+Element at index 13 :14
+Element at index 14 :15
+Element at index 15 :16
+Element at index 16 :17
+Element at index 17 :18
+Element at index 18 :19
+Element at index 19 :20
+
+*/
