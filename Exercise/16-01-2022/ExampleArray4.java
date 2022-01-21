ca4e1283414b83039611b7cb863346bd40ea9b24
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-22 01:18:02 +0530

    Add files via upload

---
+++ Exercise/16-01-2022/ExampleArray4.java
@@ -0,0 +1,12 @@
+
+public class ExampleArray4 {
+
+	public static void main(String[] args) {
+	double mydouble=9.78;
+	int myInt=(int) mydouble;
+	System.out.println(mydouble);
+	System.out.println(myInt);
+
+	}
+
+}
