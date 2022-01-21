27c9eebfdb2a9627087856a78b55c24ee8f353cb
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-22 00:54:50 +0530

    Add files via upload

---
+++ Exercise/16-01-2022/ExampleArray1.java
@@ -0,0 +1,15 @@
+
+public class ExampleArray1 {
+  static int[] get() {
+	  return new int [] {10,20,30,40,50,60};
+	  
+  }
+	public static void main(String[] args) {
+		int[]num=get();
+		for (int i=0; i<num.length; i++) {
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
