92aa7c4f1dc63df9a57e897091210bbb442e7274
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-01-22 01:22:12 +0530

    Add files via upload

---
+++ Exercise/16-01-2022/ExampleArray5.java
@@ -0,0 +1,17 @@
+
+public class ExampleArray5 {
+
+ public static void main(String[] args) {
+
+	int myInt = 9;
+	double myDouble = myInt; // Automatic casting: int to double
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+
+}
+/*
+output:
+9
+9.0
+*/
