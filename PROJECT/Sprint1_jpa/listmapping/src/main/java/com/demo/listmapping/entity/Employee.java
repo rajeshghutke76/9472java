38dc6429174cdbcd235971d5b7613fc7565344b6
rajeshghutke76 <95093731+rajeshghutke76@users.noreply.github.com>
2022-02-02 22:32:32 +0530

    Add files via upload

---
+++ PROJECT/Sprint1_jpa/listmapping/src/main/java/com/demo/listmapping/entity/Employee.java
@@ -0,0 +1,58 @@
+package com.demo.listmapping.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+@Entity
+public class Employee {
+@Id	
+@GeneratedValue(strategy=GenerationType.AUTO)
+
+    private int e_id;
+	private String e_name;
+	
+	@ElementCollection
+	private List<Address>address=new ArrayList <Address>();
+	
+	public Employee(int e_id, String e_name, List<Address> address) {
+		super();
+		this.e_id = e_id;
+		this.e_name = e_name;
+		this.address = address;
+	}
+
+	public Employee() {}
+	
+	
+	public int getE_id() {
+		return e_id;
+	}
+
+	public void setE_id(int e_id) {
+		this.e_id = e_id;
+	}
+
+	public String getE_name() {
+		return e_name;
+	}
+
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+
+	public List<Address> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+
+	
+}
